bdc54e3dc3b19d6761db6ae5278a2aeaaa5c8969
Ore Akin-Odidi <oreakinodidi@microsoft.com>
2023-11-28 16:01:02 +0000

    manual integration = false

--- modules/appservice/main.tf
+++ modules/appservice/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_app_service" "app-svc" {
 source_control {
     repo_url = "https://github.com/oreakinodidi98/Terraform-app-service"
     branch = "main"
-    manual_integration = true
+    manual_integration = false
     use_mercurial = false
 }
 #   app_settings = {
