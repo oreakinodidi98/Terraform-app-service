5066146e1c4e99e66802a4369f7b65cb06b277d7
Ore Akin-Odidi <oreakinodidi@microsoft.com>
2023-11-28 15:54:20 +0000

    changed to main

--- modules/appservice/main.tf
+++ modules/appservice/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_app_service" "app-svc" {
   }
 source_control {
     repo_url = "https://github.com/oreakinodidi98/Terraform-app-service"
-    branch = "Main"
+    branch = "main"
     manual_integration = true
     use_mercurial = false
 }
