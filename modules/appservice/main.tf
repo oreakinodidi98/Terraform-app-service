69b2b4bffdab3365b51dcecb20b7128983be2be5
Ore Akin-Odidi <oreakinodidi@microsoft.com>
2023-11-30 12:09:41 +0000

    changed app svc

--- modules/appservice/main.tf
+++ modules/appservice/main.tf
@@ -1,41 +1,53 @@
-resource "azurerm_app_service_plan" "app-svc-plan" {
-  name                = "${var.naming_prefix}-app-plan"
-  location            = var.location
-  resource_group_name = var.resourcegroup
-  kind = "Linux"
-  reserved = true
-  tags = var.tags
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_app_service" "app-svc" {
-  name                = "${var.naming_prefix}-app-service"
-  location            = var.location
-  resource_group_name = var.resourcegroup
-  app_service_plan_id = azurerm_app_service_plan.app-svc-plan.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit" 
-    remote_debugging_enabled = true
-    remote_debugging_version = "VS2019"
-  }
-# source_control {
-#     repo_url = "https://github.com/oreakinodidi98/Terraform-app-service"
-#     branch = "main"
-#     manual_integration = true
-#     use_mercurial = false
+# resource "azurerm_app_service_plan" "app-svc-plan" {
+#   name                = "${var.naming_prefix}-app-plan"
+#   location            = var.location
+#   resource_group_name = var.resourcegroup
+#   kind = "Linux"
+#   reserved = true
+#   tags = var.tags
+#   sku {
+#     tier = "Standard"
+#     size = "S1"
+#   }
 # }
-#   app_settings = {
-#     "SOME_KEY" = "some-value"
-#   }
+resource "azurerm_service_plan" "app-svc-plan" {
+  name                = "${var.naming_prefix}-app-plan"
+  resource_group_name = var.resourcegroup
+  location            = var.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+# resource "azurerm_app_service" "app-svc" {
+#   name                = "${var.naming_prefix}-app-service"
+#   location            = var.location
+#   resource_group_name = var.resourcegroup
+#   app_service_plan_id = azurerm_app_service_plan.app-svc-plan.id
 
-#   connection_string {
-#     name  = "Database"
-#     type  = "SQLServer"
-#     value = "Server=some-server.mydomain.com;Integrated Security=SSPI"
+#   site_config {
+#     dotnet_framework_version = "v4.0"
+#     scm_type                 = "LocalGit" 
+#     remote_debugging_enabled = true
+#     remote_debugging_version = "VS2019"
 #   }
+# # source_control {
+# #     repo_url = "https://github.com/oreakinodidi98/Terraform-app-service"
+# #     branch = "main"
+# #     manual_integration = true
+# #     use_mercurial = false
+# # }
+# }
+resource "azurerm_linux_web_app" "app-svc" {
+  name                = "${var.naming_prefix}-app-service"
+  resource_group_name = var.resourcegroup
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.app-svc-plan.id
+
+  site_config {}
+}
+resource "azurerm_app_service_source_control" "app-svc-git" {
+  app_id   = azurerm_linux_web_app.app-svc.id
+  repo_url = "https://github.com/oreakinodidi98/Terraform-app-service"
+  branch   = "main"
+  use_manual_integration = true
+  use_mercurial = false
 }
---
+++ output.tf
@@ -0,0 +1,33 @@
+#output resource group name
+output "resource_group_name" {
+  value = azurerm_resource_group.resourcegroup.name
+}
+# output web app name
+output "app_svc_name" {
+  value = module.appservice.app_svc_name
+}
+# output web app id
+output "app_svc_id" {
+  value = module.appservice.app_svc_id
+}
+
+# output app service plan name
+output "app_svc_plan_name" {
+  value = module.appservice.app_svc_plan_name
+}
+# output app service plan id
+output "app_svc_plan_id" {
+  value = module.appservice.app_svc_plan_id
+}
+# output log analytics workspace name
+output "log_analytics_workspace_name" {
+  value = module.loganalytics.log_analytics_workspace_name
+}
+# output log analytics workspace id
+output "log_analytics_workspace_id" {
+  value = module.loganalytics.log_analytics_workspace_id
+}
+# output source control resource id
+output "source_control_resource_id" {
+  value = module.appservice.source_control_resource_id
+}
