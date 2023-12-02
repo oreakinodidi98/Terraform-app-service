7ac4d1974ae87540f2c2c16a456279f533632988
Ore Akin-Odidi <oreakinodidi@microsoft.com>
2023-12-02 16:59:14 +0000

    removed dev source control

--- modules/appservice/output.tf
+++ modules/appservice/output.tf
@@ -16,9 +16,9 @@ output "app_svc_plan_name" {
 output "app_svc_plan_id" {
   value = azurerm_service_plan.app-svc-plan.id
 }
-# output "source_control_resource_id" {
-#   value = azurerm_app_service_source_control.app-svc-git.id
-# }
-output "dev_source_control_resource_id" {
-  value = azurerm_app_service_source_control_slot.dev-app-svc-git.id
+output "source_control_resource_id" {
+  value = azurerm_app_service_source_control.app-svc-git.id
 }
+# output "dev_source_control_resource_id" {
+#   value = azurerm_app_service_source_control_slot.dev-app-svc-git.id
+# }
